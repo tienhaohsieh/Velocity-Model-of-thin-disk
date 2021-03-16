46bf21b2c08e38061ea7de211019a587326bb361
Tien-Hao Hsieh <thhsieh@mpe.mpg.de>
2021-03-16 15:58:02 +0100

    Update

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# Disk_Vmodel
+# Disk line of sight velocity model
+
+A simple tool to visual the disk velocity at the line of sight. The model takes a simple thin disk with a Keplerian velocity field. To see the model, simply run >> python ToShow.py
+One can move the cursor to adjust the position angle, and scroll wheel to change the inclination angle.
+The position angle and inclination angle will simultanelously shown in the top-left corner.
+
+#### Requirments:
+* Numpy
+* Matplotlib
+
+### Example
+A simple model with a Keplerian rotation with a velocity=10 km/s at a radius R=10 au.
+The distance and velocity (scaled to r=10au) can be modified in the Function.py and ToShow.py.
+
+![image]()
+
--- ToShow.py
+++ ToShow.py
@@ -6,36 +6,68 @@ from Function import *
 rcParams['xtick.direction'] = 'in'
 rcParams['ytick.direction'] = 'in'
 
+Vr10=10
 vmax=5
 vmin=-5
 cmap='bwr'
-
-PAuse=33.6  # Tobin et al. 2018 for Per-emb-44B
-ellip_B=[0.32,0.2] # Tobin et al. 2018 for Per-emb-44B
-Inc_use=IncA(ellip_B[0],ellip_B[1])
-ellip_A=[0.16,0.093] # Tobin et al. 2018 for Per-emb-44A
-
+extent=bound+binn/2
 
 fig=figure(figsize=(6,5))
 ax=fig.add_subplot(1,1,1)
 
-ax.legend(fontsize='small', ncol=2,loc='upper left')
+Inc=45;PA=0
+class Cursor():
+    def __init__(self, ax):
+        self.ax = ax
+	self.Inc = 45.
+    def mouse_move(self,event):
+	ax.cla()
+	if not event.inaxes:
+	    return
+	x, y = event.xdata, event.ydata
+	self.PA=rad2deg(arctan(x/y))
+	self.PA=180+self.PA if y<0 else self.PA
+
+	Vmod=img_Model(Vr10=Vr10,xcen=0.,ycen=0.,Inc=self.Inc,PA=-self.PA,mod='Kep')
+	ax.imshow(Vmod,extent=(extent,-extent,-extent,extent),zorder=0,vmax=vmax,vmin=vmin,cmap=cmap,origin='lower')
+	ax.annotate(r'$\theta_{\rm inc}$='+'%.d'%self.Inc+'$^\circ$',xycoords='axes fraction',xy=(0.05,0.9))
+	ax.annotate(r'$PA$='+'%.d'%self.PA+'$^\circ$',xycoords='axes fraction',xy=(0.05,0.8))
+	setup(self.ax)
+	plt.draw()
+
+    def scrolling(self,event):
+	ax.cla()
+        if event.button=='up':
+          self.Inc += 2
+        elif event.button=='down':
+          self.Inc -= 2
+	Vmod=img_Model(Vr10=Vr10,xcen=0.,ycen=0.,Inc=self.Inc,PA=-self.PA,mod='Kep')
+	ax.imshow(Vmod,extent=(extent,-extent,-extent,extent),zorder=0,vmax=vmax,vmin=vmin,cmap=cmap,origin='lower')
+	ax.annotate(r'$\theta_{\rm inc}$='+'%.d'%self.Inc+'$^\circ$',xycoords='axes fraction',xy=(0.05,0.9))
+	ax.annotate(r'$PA$='+'%.d'%self.PA+'$^\circ$',xycoords='axes fraction',xy=(0.05,0.8))
+	setup(self.ax)
+	plt.draw()
+
+def setup(ax):
+	ax.set_xlim(0.4,-0.4)
+	ax.set_ylim(-0.4,0.4)
+	ax.set_xlabel(r'$\delta X$ (arcsec)')
+	ax.set_ylabel(r'$\delta Y$ (arcsec)')
+	ax.set_aspect('equal')
+	
+mouse=Cursor(ax)
+connect('motion_notify_event', mouse.mouse_move)
+connect('scroll_event', mouse.scrolling)
+
+setup(ax)
 cax=axes([0.87, 0.1, 0.03, 0.87])
+Vmod=img_Model(Vr10=10,xcen=0.,ycen=0.,Inc=Inc,PA=PA,mod='Kep')
+im=ax.imshow(Vmod,extent=(extent,-extent,-extent,extent),zorder=0,vmax=vmax,vmin=vmin,cmap=cmap,origin='lower')
 cbar=colorbar(im,cax=cax)
 cbar.set_label(r'km s$^{-1}$')
 cbar.set_ticks(arange(vmin,vmax,1))
-ax.set_xlim(0.4,-0.4)
-ax.set_ylim(-0.4,0.4)
-ax.set_xlabel(r'$\delta X$ from Per-emb-44B (arcsec)')
-ax.set_ylabel(r'$\delta Y$ from Per-emb-44B (arcsec)')
-ax.set_aspect('equal')
 subplots_adjust(left=0.15,right=0.85,bottom=0.1,top=0.97)
 
-Vmod=img_Model(Vr10=5,xcen=0.,ycen=0.,Inc=Inc_use,PA=-PAuse,mod='Kep')
-extent=bound+binn/2
-ax.imshow(Vmod,extent=(extent,-extent,-extent,extent),zorder=0,vmax=vmax,vmin=vmin,cmap=cmap,origin='lower')
-#ax.contour(Vmod,extent=(extent,-extent,-extent,extent),levels=arange(vmin,vmax,0.5),zorder=1)
 
-savefig('Per-emb-44B_Model.pdf')
 show()
 
